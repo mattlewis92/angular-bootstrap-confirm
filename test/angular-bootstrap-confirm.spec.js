62bdda23eda32463f395bb7b820146b42b9d2a2c
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-05-02 20:28:26 +0100

    Add dummy tests

--- test/angular-bootstrap-confirm.spec.js
+++ test/angular-bootstrap-confirm.spec.js
@@ -2,13 +2,153 @@ describe('Confirm popover', function() {
 
   describe('PopoverConfirmController', function() {
 
-    it('should be a dummy test', function() {
-      expect(true).to.be.true;
+    describe('showPopover', function() {
+
+      it('should show the popover', function() {
+
+      });
+
+    });
+
+    describe('hidePopover', function() {
+
+      it('should hide the popover', function() {
+
+      });
+
+    });
+
+    describe('togglePopover', function() {
+
+      it('should show the popover if hidden', function() {
+
+      });
+
+      it('should hide the popover if visible', function() {
+
+      });
+
+    });
+
+    describe('element click', function() {
+
+      it('should toggle the popover when the element is clicked', function() {
+
+      });
+
+    });
+
+    describe('window resize', function() {
+
+      it('should reposition the popover when the window resizes', function() {
+
+      });
+
+    });
+
+    describe('$destroy', function() {
+
+      it('should remove the popover when the scope is destroyed', function() {
+
+      });
+
     });
 
   });
 
   describe('mwlConfirmDirective', function() {
+
+    it('should set the default confirm text', function() {
+
+    });
+
+    it('should set the confirm text when specified', function() {
+
+    });
+
+    it('should set the default cancel text', function() {
+
+    });
+
+    it('should set the cancel text when specified', function() {
+
+    });
+
+    it('should set the popover message', function() {
+
+    });
+
+    it('should allow html in the popover message', function() {
+
+    });
+
+    it('should set the popover title', function() {
+
+    });
+
+    it('should allow html in the popover title', function() {
+
+    });
+
+    it('should set the default placement to top', function() {
+
+    });
+
+    it('should set the placement when specified', function() {
+
+    });
+
+    it('should call onConfirm when the confirm button is clicked', function() {
+
+    });
+
+    it('should call onCancel when the cancel button is clicked', function() {
+
+    });
+
+    it('should set the default confirm button class to danger', function() {
+
+    });
+
+    it('should allow html in the confirm button text', function() {
+
+    });
+
+    it('should set confirm button class when specified', function() {
+
+    });
+
+    it('should set the default cancel button class to default', function() {
+
+    });
+
+    it('should set cancel button class when specified', function() {
+
+    });
+
+    it('should allow html in the cancel button text', function() {
+
+    });
+
+    it('should close the popover when another element that isn\'t the popover is clicked', function() {
+
+    });
+
+    it('should keep the popover open when an element inside the popover is clicked', function() {
+
+    });
+
+    it('should close the popover when the confirm button is clicked', function() {
+
+    });
+
+    it('should close the popover when the cancel button is clicked', function() {
+
+    });
+
+    it('should not throw any errors when the default interpolation symbols are changed', function() {
+
+    });
 
   });
 
