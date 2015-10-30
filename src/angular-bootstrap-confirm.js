e8144ef4bcc7f7a2ed9bc6b166652712f3ed0a56
Scott Bos <s.bos@kunzleigh.com>
2015-10-30 15:30:37 -0400

    Revert "Added ability to override template and auto focus the confirmation button by default."
    This reverts commit 3a301cf5089a72cc0594fdecb9e016cc6228cf40.

--- src/angular-bootstrap-confirm.js
+++ src/angular-bootstrap-confirm.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var angular = require('angular');
 require('angular-sanitize');
 require('./ui-bootstrap-position');
@@ -17,7 +18,7 @@ module.exports = angular
     var positionServiceName = $injector.has('$uibPosition') ? '$uibPosition' : '$position';
     var positionService = $injector.get(positionServiceName);
 
-    var template = angular.isDefined(vm.defaults.template) ? vm.defaults.template : [
+    var template = [
       '<div class="popover" ng-class="vm.popoverPlacement">',
         '<div class="arrow"></div>',
         '<h3 class="popover-title" ng-bind-html="vm.title"></h3>',
@@ -25,7 +26,7 @@ module.exports = angular
           '<p ng-bind-html="vm.message"></p>',
           '<div class="row">',
             '<div class="col-xs-6">',
-              '<button class="btn btn-block confirm-button" ng-class="\'btn-\' + (vm.confirmButtonType || vm.defaults.confirmButtonType)" ' +
+              '<button class="btn btn-block" ng-class="\'btn-\' + (vm.confirmButtonType || vm.defaults.confirmButtonType)" ' +
               'ng-click="vm.onConfirm(); vm.hidePopover()" ng-bind-html="vm.confirmText || vm.defaults.confirmText"></button>',
             '</div>',
             '<div class="col-xs-6">',
@@ -62,7 +63,7 @@ module.exports = angular
       if (!vm.isVisible) {
         popover.css({display: 'block'});
         positionPopover();
-        applyFocus(popover.find('button.confirm-button'));
+        applyFocus(popover.find('button'));
         vm.isVisible = true;
       }
       vm.isOpen = true;
